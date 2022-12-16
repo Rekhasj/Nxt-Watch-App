34c08f8f98c198da92222b72450517411b270156
Rekhasj <rekhasj21@gmail.com>
2022-12-16 16:20:20 +0000

    nxt-watch

---
+++ src/context/UserContext.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserContext = React.createContext({
+  isDarkTheme: false,
+  savedVideos: [],
+  activeTab: 'Home',
+  toggleTheme: () => {},
+  changeTab: () => {},
+  addVideo: () => {},
+})
+
+export default UserContext
